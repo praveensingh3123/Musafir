48d44802b50a8f9cc7bddd4147997b06a557a6fc
praveensingh3123 <praveensingh3123@gmail.com>
2021-07-06 07:24:31 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import BrowserRouter from 'react-router-dom/BrowserRouter';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    (<BrowserRouter basename={process.env.PUBLIC_URL}>
+        <App />
+      </BrowserRouter>), 
+      (document.getElementById('root'))
+    );
